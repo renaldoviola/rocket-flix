a9080f9e21447e7d2caff01da2bc843a5a6812d8
Renaldo Viola <renaldo@Rocketlab.com.br>
2020-07-27 23:53:56 -0300

    publish initial project

---
+++ src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../../assets/img/logo.png';
+import './menu.css';
+import {ButtonStyles} from './components/Button/';
+
+function Menu () {
+  return (
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="RocketFlix" />
+      </a>
+
+      <ButtonStyles className="ButtonLink" href="/">
+        Novo vídeo
+      </ButtonStyles>
+    </nav>
+  )
+
+}
+
+export default Menu;
